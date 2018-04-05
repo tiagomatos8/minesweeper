d3c326f2859578eb9ff3318b053d9124dbe6d3b8
Tiago C M Matos <tcm.matos@gmail.com>
2018-04-05 18:55:10 +1000

    Add files via upload

---
+++ src/minesweeper.js
@@ -0,0 +1,20 @@
+const board = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
+];
+
+const printBoard = board => {
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+}
+
+printBoard(board);
+
+console.log("\n");
+
+board[0].splice(1, 1, '1');
+board[2].splice(2, 1, 'B');
+
+printBoard(board);

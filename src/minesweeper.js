b837e215568fdadaceddd0f292d503629fcd7f2f
Tiago C M Matos <tcm.matos@gmail.com>
2018-04-08 12:14:03 +1000

    creating player board dinamically

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,22 +1,17 @@
 //autor: Tiago Cesar de Morais Matos
 
-const board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' ']
-];
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
 
-const printBoard = board => {
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    let row = [];
+
+    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
 }
 
-printBoard(board);
-
-console.log("\n");
-
-board[0].splice(1, 1, '1');
-board[2].splice(2, 1, 'B');
-
-printBoard(board);
+console.log(generatePlayerBoard(3,3));
